7976deb8c65b08eff86194916d600100f39d257d
unknown <ashokreddyscios@example.com>
2019-07-14 03:41:21 +0530

    all the connections

--- app/src/main/java/nihanth/com/drreddy/LoginActivity.java
+++ app/src/main/java/nihanth/com/drreddy/LoginActivity.java
@@ -1,6 +1,7 @@
 package nihanth.com.drreddy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +13,17 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,7 +32,10 @@ public class LoginActivity extends AppCompatActivity {
     private Button login;
     private Button reg;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db ;
     private ProgressBar pbar;
+    String uid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +46,15 @@ public class LoginActivity extends AppCompatActivity {
         login=(Button) findViewById(R.id.regSigninbtn);
         pbar=(ProgressBar) findViewById(R.id.regProgress);
         mAuth=FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String eid=loginEmail.getText().toString();
+                final String eid=loginEmail.getText().toString();
                 String pwd=password.getText().toString();
                 if(!TextUtils.isEmpty(eid) && !TextUtils.isEmpty(pwd))
                 {
+
                     pbar.setVisibility(View.VISIBLE);
                     mAuth.signInWithEmailAndPassword(eid,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>()
                     {
@@ -50,9 +63,41 @@ public class LoginActivity extends AppCompatActivity {
                         {
 
                             if(task.isSuccessful()){
-                                Log.i("dsjfdsgh","hzdgvhdschXZBjb");
+                                db.collection("Users").document(eid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()){
+                                            if(!task.getResult().exists()){
+                                                Map<String, Object> userMap = new HashMap<>();
+                                                userMap.put("email",eid);
+                                                userMap.put("name","        ");
+                                                userMap.put("phone","        ");
+                                                userMap.put("image"," ");
+                                                db.collection("Users").document(eid).set(userMap).addOnCompleteListener(
+                                                        new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    sendToHome(eid);
+                                                                    Toast.makeText(LoginActivity.this, "Stored in Firestore ", Toast.LENGTH_LONG).show();
 
-                                sendToHome();
+                                                                } else {
+                                                                    String err = task.getException().getMessage();
+                                                                    Toast.makeText(LoginActivity.this, "FIrestore Error:" + err, Toast.LENGTH_LONG).show();
+                                                                }
+                                                            }
+                                                        });
+
+                                            }
+                                            else{
+                                                sendToHome(eid);
+                                            }
+                                        }
+
+                                    }
+                                });
+
+
 
                             }
                             else
@@ -69,9 +114,11 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void sendToHome(){
-        Intent mainintent=new Intent(LoginActivity.this,HomeActivity.class);
+    private void sendToHome(String eid){
+        Intent mainintent=new Intent(LoginActivity.this,Catalogue.class);
+        mainintent.putExtra("email",eid);
         startActivity(mainintent);
         finish();
     }}
+
 
