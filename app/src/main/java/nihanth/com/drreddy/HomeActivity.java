21d394bbd369705a07c12cf2e2fe8ab6c0393345
unknown <ashokreddyscios@example.com>
2019-07-13 19:11:13 +0530

    Login done

---
+++ app/src/main/java/nihanth/com/drreddy/HomeActivity.java
@@ -0,0 +1,35 @@
+package nihanth.com.drreddy;
+
+import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private android.support.v7.widget.Toolbar mainToolbar;
+    private FloatingActionButton addpostButton;
+    private FirebaseFirestore db;
+    private String curid;
+    private BottomNavigationView mainbottomNav;
+    private GridView simpleGrid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+        db= FirebaseFirestore.getInstance();
+        mAuth= FirebaseAuth.getInstance();
+        mainToolbar=(android.support.v7.widget.Toolbar) findViewById(R.id.main_toolbar);
+        simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
+        // Create an object of CustomAdapter and set Adapter to GirdView
+
+    }
+}
---
+++ app/src/main/java/nihanth/com/drreddy/LoginActivity.java
@@ -0,0 +1,77 @@
+package nihanth.com.drreddy;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail;
+    private EditText password;
+    private Button login;
+    private Button reg;
+    private FirebaseAuth mAuth;
+    private ProgressBar pbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginEmail=(EditText) findViewById(R.id.regEmail);
+        password =(EditText) findViewById(R.id.regPassword);
+        login=(Button) findViewById(R.id.regSigninbtn);
+        pbar=(ProgressBar) findViewById(R.id.regProgress);
+        mAuth=FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eid=loginEmail.getText().toString();
+                String pwd=password.getText().toString();
+                if(!TextUtils.isEmpty(eid) && !TextUtils.isEmpty(pwd))
+                {
+                    pbar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(eid,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+
+                            if(task.isSuccessful()){
+                                Log.i("dsjfdsgh","hzdgvhdschXZBjb");
+
+                                sendToHome();
+
+                            }
+                            else
+                            {
+                                String error=task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this,"Error:"+error,Toast.LENGTH_LONG).show();
+                            }
+
+                            pbar.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void sendToHome(){
+        Intent mainintent=new Intent(LoginActivity.this,HomeActivity.class);
+        startActivity(mainintent);
+        finish();
+    }}
+
