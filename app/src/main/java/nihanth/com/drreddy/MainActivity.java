df28937a413b2c39320df51bf11fc14d3fb968ae
lohitha reddy <lohithareddymallakuntla@gmal.com>
2019-07-14 07:48:03 +0530

    Surveys which include feedback and courses taken as well

--- app/src/main/java/nihanth/com/drreddy/MainActivity.java
+++ app/src/main/java/nihanth/com/drreddy/MainActivity.java
@@ -140,13 +140,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.quizzes) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.upload) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.survey) {
+            Intent myIntent = new Intent(MainActivity.this, Surveys.class);
+            startActivityForResult(myIntent, 0);
+        } else if (id == R.id.tools) {
 
         } else if (id == R.id.nav_share) {
 
