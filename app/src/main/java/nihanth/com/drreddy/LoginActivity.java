fa7ab36caa1343071a692a4e02cc7f13490006d3
jyothsna <mandepudijyothsna@gmail.com>
2019-07-14 05:46:23 +0530

    next commit

--- app/src/main/java/nihanth/com/drreddy/LoginActivity.java
+++ app/src/main/java/nihanth/com/drreddy/LoginActivity.java
@@ -42,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
                 String pwd=password.getText().toString();
                 if(!TextUtils.isEmpty(eid) && !TextUtils.isEmpty(pwd))
                 {
-                    pbar.setVisibility(View.VISIBLE);
+                    pbar.setVisibility(View.INVISIBLE);
                     mAuth.signInWithEmailAndPassword(eid,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>()
                     {
                         @Override
